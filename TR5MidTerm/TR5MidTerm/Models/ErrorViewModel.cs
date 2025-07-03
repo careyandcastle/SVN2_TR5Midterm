f626d3f186733d770ee15b61f26ebf6e1b94656c
a08844 <a08844@DESKTOP-077TT8P>
2025-07-03 11:19:49 +0800

    作業初始化

---
+++ TR5MidTerm/TR5MidTerm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TR5MidTerm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
